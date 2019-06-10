c84b27606df1f018888861bf758a1c0984d490b0
Serhiy Savchuk and Wei Kuang <serhiy@>
2019-06-10 14:13:59 -0700

    edited file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 $(document).ready(function(){
+  $("button").click(function() {
 
- $(".fav").submit(function(event){
 
-   var text1 =
-   $ (".form-control1").val();
-   var text2 =
-   $(".form-control2").val();
-   var text3 =
-   $(".form-control3").val();
 
-   var textCombined = [];
-   textCombined.push(text1,text2,text3);
+  var flavors = ["chocolat", "vanilla","strawberry", "cherry"];
+  flavors.forEach(function(flavor){
 
-   $("#list1").show();
-   $("#1").append(textCombined[0]);
-   $("#2").append(textCombined[1]);
-   $("#3").append(textCombined[2]);
+    $("ul").append("<li>"+ flavor+"</li>");
+   //  $("#1").append(textCombined[0]);
+   // $("#2").append(textCombined[1]);
+   // $("#3").append(textCombined[2]);
+
 
     event.preventDefault();
+
+  });
  });
 });
